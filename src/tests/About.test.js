2099f11af8928ae6a4179578e9834fb94075877c
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-08-16 16:52:27 -0300

    Requisito2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,24 @@
-test('', () => {});
-//
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../pages/About';
+
+describe('Teste App', () => {
+  it('sse tem um elemento h1 com o texto About Pokédex', async () => {
+    render(<About />);
+    const about = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(about).toBeInTheDocument();
+  });
+  it('se tem dois paragrafos', async () => {
+    render(<About />);
+    const p1 = screen.getByText(/This application/i);
+    expect(p1).toBeInTheDocument();
+    const p2 = screen.getByText(/filter Pokémons/i);
+    expect(p2).toBeInTheDocument();
+  });
+  it('se utiliza o src correto', async () => {
+    render(<About />);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img', { name: /Pokédex/i });
+    expect(img).toHaveAttribute('src', url);
+  });
+});
