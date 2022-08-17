302bf44133ee47e04358530fbd797c1a50b909da
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-08-17 11:45:16 -0300

    Requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste PokemonDetails', () => {
+  it('se informações do pokemon selecionado são mostradas', () => {
+    renderWithRouter(<App />);
+    const moreDetail = screen.queryByRole('link', { name: /more Detail/i });
+    userEvent.click(moreDetail);
+    const name = screen.getByText(/Pikachu Details/i);
+    expect(name).toBeInTheDocument();
+    expect(moreDetail).not.toBeInTheDocument();
+    const text = screen.getByRole('heading', { level: 2, name: /Summary/i });
+    expect(text).toBeInTheDocument();
+    const pokemonDetail = screen.getByText(/roasts hard berries with electricity/i);
+    expect(pokemonDetail).toBeInTheDocument();
+  });
+  it('se informações sobre as localizações do pokemon são mostradas', () => {
+    renderWithRouter(<App />);
+    const moreDetail = screen.queryByRole('link', { name: /more Detail/i });
+    userEvent.click(moreDetail);
+    const textLocation = screen.getByRole('heading', { level: 2,
+      name: 'Game Locations of Pikachu' });
+    expect(textLocation).toBeInTheDocument();
+    const locations = [{
+      location: 'Kanto Viridian Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    },
+    {
+      location: 'Kanto Power Plant',
+      map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    }];
+    locations.forEach(({ location, map }, index) => {
+      const pokemonLocation = screen.getByText(location);
+      expect(pokemonLocation).toBeInTheDocument();
+      const img = screen.getAllByAltText(/Pikachu location/i);
+      expect(img[index]).toHaveAttribute('src', map);
+    });
+  });
+  it('se tem um checkbox que da para favoritar', () => {
+    renderWithRouter(<App />);
+    const moreDetail = screen.queryByRole('link', { name: /more Detail/i });
+    userEvent.click(moreDetail);
+    const checkbox = screen.getByLabelText(/Pokémon favoritado?/i);
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox.checked).toBe(false);
+    userEvent.click(checkbox);
+    expect(checkbox.checked).toBe(true);
+  });
+});
